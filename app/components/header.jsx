5dea8a9dbed7707a092892a46992fc879989b799
Victor Seleimend <vsennaselei@gmail.com>
2023-02-01 10:47:11 -0300

    Ativando o link de acordo com o pathname

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,20 +1,41 @@
-import { Link } from '@remix-run/react';
+import { Link, useLocation } from '@remix-run/react';
 import logo from '../../public/img/logo.svg';
 
-const Header = () => (
-  <header className="header">
-    <div className="container bar">
-      <Link to='/' className="logo">
-        <img className="logo" src={logo} alt="logo" />
-      </Link>
-      <nav className="navigation">
-        <Link to="/">Home</Link>
-        <Link to="/about-us">Sobre Nós</Link>
-        <Link to="/store">Loja</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
-    </div>
-  </header>
-);
+const Header = () => {
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <div className="container bar">
+        <Link to="/" className="logo">
+          <img className="logo" src={logo} alt="logo" />
+        </Link>
+        <nav className="navigation">
+          <Link className={location.pathname === '/' ? 'active' : ''} to="/">
+            Home
+          </Link>
+          <Link
+            className={location.pathname === '/about-us' ? 'active' : ''}
+            to="/about-us"
+          >
+            Sobre Nós
+          </Link>
+          <Link
+            className={location.pathname === '/store' ? 'active' : ''}
+            to="/store"
+          >
+            Loja
+          </Link>
+          <Link
+            className={location.pathname === '/blog' ? 'active' : ''}
+            to="/blog"
+          >
+            Blog
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
