b453f870f73a2ce27a0d00aa981f1bdc7ad079e7
Victor Seleimend <vsennaselei@gmail.com>
2023-02-01 12:19:05 -0300

    Obtendo as guitarras do Strapi

--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,3 +1,10 @@
+export const loader = async () => {
+  const response = await fetch(`${process.env.API_URL}/guitars?populate=image`);
+  const result = await response.json();
+
+  return result;
+};
+
 const Store = () => <h1>Store</h1>;
 
 export default Store;
