b50a379b085d32688926104f6f9d6b53ada8a427
Victor Seleimend <vsennaselei@gmail.com>
2023-02-01 12:52:32 -0300

    Consumindo guitarras

---
+++ app/components/guitar.jsx
@@ -0,0 +1,5 @@
+const Guitar = ({ guitar }) => {
+  return <h1>Guitar</h1>;
+};
+
+export default Guitar;
--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,14 +1,31 @@
 import { useLoaderData } from '@remix-run/react';
+
 import { getGuitars } from '~/models/guitars.server';
+
+import Guitar from '~/components/guitar';
 
 export const loader = async () => {
   const guitars = await getGuitars();
 
-  return guitars;
+  return guitars.data;
 };
 
 const Store = () => {
-  return <h1>Store</h1>
+  const guitars = useLoaderData();
+
+  return (
+    <main className="container">
+      <h2 className="heading">Nossa Coleção</h2>
+
+      {guitars?.length && (
+        <div className="guitars-grid">
+          {guitars.map(guitar => (
+            <Guitar guitar={guitar?.attributes} key={guitar?.id} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Store;
