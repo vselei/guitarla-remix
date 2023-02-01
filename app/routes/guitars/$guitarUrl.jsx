da7455e7e1fd6c59f1564f333e79ee627d46717e
Victor Seleimend <vsennaselei@gmail.com>
2023-02-01 17:55:06 -0300

    Exibindo guitarras nas rotas especificas

--- app/routes/guitars/$guitarUrl.jsx
+++ app/routes/guitars/$guitarUrl.jsx
@@ -1,4 +1,14 @@
+import { useLoaderData } from '@remix-run/react';
 import { getGuitar } from '~/models/guitars.server';
+
+import styles from '~/styles/guitars.css';
+
+export const links = () => [
+  {
+    rel: 'stylesheet',
+    href: styles
+  }
+];
 
 export const loader = async ({ params }) => {
   const { guitarUrl } = params;
@@ -9,7 +19,23 @@ export const loader = async ({ params }) => {
 };
 
 const Guitar = () => {
-  return <h1>Guitar</h1>;
+  const guitar = useLoaderData();
+  const { name, description, image, price } = guitar.data[0].attributes;
+
+  return (
+    <main className="container guitar">
+      <img
+        className="image"
+        src={image.data.attributes.url}
+        alt={`Imagem da guitarra ${name}`}
+      />
+      <div className="content">
+        <h3>{name}</h3>
+        <p className="text">{description}</p>
+        <p className="price">${price}</p>
+      </div>
+    </main>
+  );
 };
 
 export default Guitar;
