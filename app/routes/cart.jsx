7e670144f9b5eab86a547c2cd2c46096dc55b0ec
Victor Seleimend <vsennaselei@gmail.com>
2023-02-02 12:41:39 -0300

    Estilização do carrinho

--- app/routes/cart.jsx
+++ app/routes/cart.jsx
@@ -1,5 +1,32 @@
+import styles from '~/styles/cart.css';
+
+export const meta = () => ({
+  title: 'GuitarLA | Carrinho',
+  description: 'Venda de guitarras e blog. Carrinho de compras'
+});
+
+export const links = () => [
+  {
+    rel: 'stylesheet',
+    href: styles
+  }
+];
+
 const Cart = () => {
-  return <h1>Hello</h1>
-}
+  return (
+    <main className="container">
+      <h1 className="heading">Carrinho de Compras</h1>
+      <div className="content">
+        <div className="cart">
+          <h2>Itens</h2>
+        </div>
+        <aside className="summary">
+          <h3>Resumo do Pedido</h3>
+          <p>Total a pagar: $</p>
+        </aside>
+      </div>
+    </main>
+  );
+};
 
 export default Cart;
